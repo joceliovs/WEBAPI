a99529b5db32f0d947bad5562c2eb84d74d33bf8
Jovs <joceliovieira@gmail.com>
2023-04-27 11:55:03 -0300

    first commit

---
+++ src/main/java/Jovs/WEBAPI/WebapiApplication.java
@@ -0,0 +1,13 @@
+package Jovs.WEBAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapiApplication.class, args);
+	}
+
+}
